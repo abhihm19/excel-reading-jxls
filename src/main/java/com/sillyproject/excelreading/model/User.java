b52e490789d236891c7fdea60c99be9b5faf99a4
Abhishek Mat <amat@osius.com>
2023-08-20 19:49:18 +0530

    update-read-success

--- src/main/java/com/sillyproject/excelreading/model/User.java
+++ src/main/java/com/sillyproject/excelreading/model/User.java
@@ -1,7 +1,5 @@
 package com.sillyproject.excelreading.model;
 
-import java.time.LocalDate;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +12,5 @@ public class User {
 	private int serialNumber;
 	private String name;
 	private int score;
-	private String dateOfBirth;
 	private long mobileNumber;
 }
